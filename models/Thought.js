aade12391617756afc260e5f68c8982f1196fe22
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2023-02-27 17:23:29 -0800

    file structure and models

---
+++ models/Thought.js
@@ -0,0 +1,72 @@
+const { Schema, model, Types } = require('mongoose');
+
+
+// schema to create reaction document that will be nested in the thought model
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+    
+)
+
+// schema to create thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max: 280,
+
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// virtual property (not stored in MongoDB) of a user's friend count
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+// create User model with schema
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+
+// schema to create user model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, "Please enter a vaild email address."],
+        },
+        thoughts: [ 
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId, 
+            ref: 'User',
+        }
+    ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// virtual property (not stored in MongoDB) of a user's friend count
+userSchema.virtual('friendcount').get(function () {
+    return this.friends.length;
+})
+
+// create User model with schema
+const User = model('user', userSchema);
+
+module.exports = User;
+
